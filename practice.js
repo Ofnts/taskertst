dae5d59f9d3ca19f81ef844620bdb8e94d3847ff
Offey <102432828+Ofnts@users.noreply.github.com>
2022-05-09 18:53:54 +0200

    Add files via upload
    first commit

---
+++ practice.js
@@ -0,0 +1,75 @@
+//---------------------------Get the required elements from the DOM-----------
+
+const input = document.querySelector(".in")
+const data = document.querySelector("#out")
+const add = document.querySelector(".add")
+const del = document.querySelector(".del")
+
+//------------------------------Create Elements and logic functions--------------
+
+function createElements(e){
+	e = input.value
+	const list = document.createElement('li')
+	const bin = document.createElement('img')
+	const box = document.createElement('input')
+	const time = document.createElement('p')
+	const done = document.createElement('img')
+	const container = document.createElement('div')
+	done.setAttribute("src", "./ok.svg")
+	time.textContent = "created " + new Date()
+	time.append(done)
+	list.textContent = e
+	list.append(time)
+	bin.setAttribute("src", "./trash.svg")
+	box.setAttribute("type", "checkbox")
+	
+	
+
+	container.appendChild(list)
+	container.prepend(bin)
+	container.append(box)
+	data.append(container)
+	container.querySelectorAll(".save")
+	input.value = ""
+
+//--------------------Check to highlight when functions are completed----------------------
+
+function completedtask(){
+	container.onchange = function(d){
+		if(d.target.checked){
+			list.style.textDecoration = "line-through"
+			list.style.color = "red"
+		}else{
+			list.style.textDecoration = "none"
+			list.style.color = "white"
+		}
+	}
+}
+
+function manageInputField(){
+	if(e == ""){
+		data.removeChild(container)
+		alert("Write somethng")
+		}
+	}
+
+
+//------------------Save Elements to the localStorage----Load Function------Delete using Bin
+
+	bin.onclick = () => {data.removeChild(container)}
+	completedtask()
+	manageInputField()
+
+}
+//---------------------------------------Add Events to the window----------------------------
+
+
+function addToScreen(){
+	add.addEventListener('click', createElements)
+	input.addEventListener('keypress', function(){
+		if(event.key === "Enter"){createElements()} 
+	})
+}
+addToScreen()
+
+
